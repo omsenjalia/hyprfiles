b62f0c6f1bd653838a444ff5526df5db04036b98
om <omsenjalia@gmail.com>
2025-03-22 12:15:57 +0530

    messed up big time

--- install/includes/links.sh
+++ install/includes/links.sh
@@ -1,1 +1,9 @@
-ln -s ~/hyprfiles/dotfiles/conf ~/.config
+figlet Creating Sysmlinks
+
+ln -sf ~/hyprfiles/dotfiles/.config/fastfetch ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/alacritty ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/fish ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/hypr ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/hyprpanel ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/rofi ~/.config
+ln -sf ~/hyprfiles/dotfiles/.config/wlogout ~/.config
